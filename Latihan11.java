05fb59f5d3cded5461145b4bab29aeff63625efa
Excell Christian <excellchriatian12@gmail.com>
2024-09-03 19:35:04 +0700

    Add files via upload

---
+++ Latihan11.java
@@ -0,0 +1,14 @@
+/**
+ * Latihan11
+ */
+public class Latihan11 {
+    public static void main(String[] args) {
+       System.out.println("        IDENTITAS DIRI           ");
+       System.out.println("NAMA     : EXCELL CHRISTIAN      ");
+       System.out.println("ABSEN    : 11                    ");
+       System.out.println("KELAS    : 1I                    ");
+       System.out.println("PRODI    : TEKNIK INFORMATIKA    ");
+       System.out.println("JURUSAN  : TEKNOLOGI INFORMASI   ");
+    }
+       
+   }
---
+++ MyFirstJava11.java
@@ -0,0 +1,6 @@
+public class MyFirstJava11 {
+ public static void main(String[] args) {
+    System.out.println("Halo Cantik Nama Saya Excell");
+ }
+    
+}
